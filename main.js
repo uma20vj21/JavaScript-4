faa426f9e0fd83f018724828c5a8b8579e5ddb35
大嶋由真 <>
2022-06-19 14:50:13 +0900

    1-4修正完了

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-const first = () => {
-  const charts = [
-    '====================',
-    '現在持っているのタスク一覧',
-    '====================',
-  ];
-  charts.forEach((chart) => console.log(chart));
-};
+const charts = [
+  '====================',
+  '現在持っているのタスク一覧',
+  '====================',
+];
+
 // 関数の中に実行したいものを定義した。かつ配列の中にオブジェクトを入れ、復路を一つ一つ取り出していくことをイメージ。
 const todos = [
   {
@@ -21,13 +19,13 @@ const todos = [
     genre: '運動',
   },
 ];
-const showTodo = () => {
+const showTodos = () => {
+  charts.forEach((chart) => console.log(chart));
   todos.forEach((todo) =>
     console.log(`[内容]: ${todo.content}、[ジャンル]${todo.genre}`)
   );
 };
-first();
-showTodo();
+showTodos();
 const content = prompt('タスクを入力してください');
 const genre = prompt('ジャンルを入力してください');
 alert('新しいタスクを追加しました。');
@@ -40,7 +38,5 @@ const addTodo = () => {
   };
   todos.push(todo);
 };
-
-first();
 addTodo();
-showTodo();
+showTodos();
